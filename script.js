50db218905fea519ab9e07121da4139156525d3d
Radar <84771532+radar-love@users.noreply.github.com>
2025-07-31 23:04:19 -0230

    update script.js

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const left = document.getElementById("left");
 const right = document.getElementById("right");
 const container = document.querySelector(".container");
 
+let activePanel = null;
+
 left.addEventListener("click", () => {
-  container.classList.remove("expand-right");
-  container.classList.add("expand-left");
+  if (activePanel === "left") {
+    container.classList.remove("expand-left");
+    activePanel = null;
+  } else {
+    container.classList.remove("expand-right");
+    container.classList.add("expand-left");
+    activePanel = "left";
+  }
 });
 
 right.addEventListener("click", () => {
-  container.classList.remove("expand-left");
-  container.classList.add("expand-right");
+  if (activePanel === "right") {
+    container.classList.remove("expand-right");
+    activePanel = null;
+  } else {
+    container.classList.remove("expand-left");
+    container.classList.add("expand-right");
+    activePanel = "right";
+  }
 });
